a654051ed701afe7b246a0e4af634add1839394c
Jalmir Reis <105806129+Jalmir-Reis@users.noreply.github.com>
2022-10-18 16:59:11 +0100

    Update settings.py

--- Modelo/settings.py
+++ Modelo/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rha^k4-2&c1*$9-av@pv4l402f*f5_nfl8a^ww=fgkqni(@va+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'cadastro-rpg.render.com']
 
 
 # Application definition
